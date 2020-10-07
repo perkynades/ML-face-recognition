4e3942d4b7371f042f7b74522fdf8156714ef6b0
Emil <emil.nilsen9888@gmail.com>
2020-10-07 21:58:34 +0200

    New template

--- src/main.py
+++ src/main.py
@@ -1,25 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets.olivetti_faces import fetch_olivetti_faces
-from sklearn.model_selection import train_test_split
 
-# Fetching faces data, and converting faces data from matrix to vector 
-data = np.load("datasets/olivetti_faces.npy")
-target = np.load("datasets/olivetti_faces.npy")
+# Fetch data and have a look
+faces = fetch_olivetti_faces()
+x, y = faces['data'], faces['target']
+print(f'Data shape: {x.shape}')
+print(f'Label shape: {y.shape}')
+# (400, 4096)
+# (400,)
 
-X = data.reshape((data.shape[0], data.shape[1] * data.shape[2]))
-print("X shape:", X.shape)
+# create a grid of 3x3 images
+for i in range(0, 9):
+  plt.subplot(330 + 1 + i)
+  plt.imshow(x[i].reshape(64, 64))
 
-# Divide data into training, validation and testing (shuffle)
+# show the plot
+plt.show()
 
-# Use PCA for diensinality reduction - reconstruct images using a subset of features
+# divide data into training, validation and testing (shuffle)
 
-# Use autoencode - reconstruct using a compressed representation (code)
+# use PCA for diensionality reduction - reconstruct images using a subset of features
 
-# Unsupervised algorithm
+# use autoencoder - reconstruct using a compressed representation (code)
 
-# Supervised algorithm
+# unsupervised algorithm
 
-# Confusion matrix
+# supervised algorithm
 
-# Cross - validation - SGD, BGD and MBGD
+# confusion matrix
+
+#cross-validation  - SGD, BGD and MBGD
